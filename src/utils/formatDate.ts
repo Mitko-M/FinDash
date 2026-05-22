2293dfc33514d2effef1c532909399bff0c7c8d7
Mitko Mitkov <mitkomitkov2727@gmail.com>
2026-05-23 00:58:42 +0300

    Added date formatter

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,8 @@
+//The formatted date will look like this: Feb 15, 2026
+export function formatDate(date: Date) {
+  return date.toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+}
