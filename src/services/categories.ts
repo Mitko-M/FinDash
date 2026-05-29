9102cc720f9534248a3b0fe8524abbb82e1b05c0
Mitko Mitkov <mitkomitkov2727@gmail.com>
2026-05-30 00:08:23 +0300

    Added a Record of all expense categories with an icon

---
+++ src/services/categories.ts
@@ -0,0 +1,24 @@
+import { CategoryType } from "@/src/types/Category";
+import { Ionicons } from "@expo/vector-icons";
+
+export const Categories: Record<
+  CategoryType,
+  { label: string; icon: keyof typeof Ionicons.glyphMap; color: string }
+> = {
+  Food: { label: "Food & Dining", icon: "fast-food", color: "#FF7043" },
+  Transport: { label: "Transport", icon: "car", color: "#42A5F5" },
+  Shopping: { label: "Shopping", icon: "cart", color: "#AB47BC" },
+  Bills: { label: "Bills & Utilities", icon: "flash", color: "#FFA726" },
+  Entertainment: {
+    label: "Entertainment",
+    icon: "game-controller",
+    color: "#26C6DA",
+  },
+  HealthCare: { label: "HealthCare", icon: "fitness", color: "#EF5350" },
+  Education: { label: "Education", icon: "school", color: "#66BB6A" },
+  Other: {
+    label: "Other Expense",
+    icon: "ellipsis-horizontal",
+    color: "#BDBDBD",
+  },
+};
---
+++ src/types/Category.ts
@@ -0,0 +1,9 @@
+export type CategoryType =
+  | "Food"
+  | "Transport"
+  | "Shopping"
+  | "Bills"
+  | "Entertainment"
+  | "HealthCare"
+  | "Education"
+  | "Other";
