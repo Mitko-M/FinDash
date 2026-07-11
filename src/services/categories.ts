6fe17fcee225e4cd6321a4d5315675b6458777df
Mitko Mitkov <mitkomitkov2727@gmail.com>
2026-07-11 17:30:10 +0300

    Added gradient to the categories metadata

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -3,23 +3,65 @@ import { Ionicons } from "@expo/vector-icons";
 
 export const Categories: Record<
   CategoryType,
-  { label: string; icon: keyof typeof Ionicons.glyphMap; color: string }
+  {
+    label: string;
+    icon: keyof typeof Ionicons.glyphMap;
+    color: string;
+    gradient: string;
+  }
 > = {
-  Food: { label: "Food & Dining", icon: "fast-food", color: "#FF7043" },
-  Transport: { label: "Transport", icon: "car", color: "#42A5F5" },
-  Shopping: { label: "Shopping", icon: "cart", color: "#AB47BC" },
-  Bills: { label: "Bills & Utilities", icon: "flash", color: "#FFA726" },
+  Food: {
+    label: "Food & Dining",
+    icon: "fast-food",
+    color: "#FF7043",
+    gradient: "#D84315",
+  },
+  Transport: {
+    label: "Transport",
+    icon: "car",
+    color: "#42A5F5",
+    gradient: "#1E88E5",
+  },
+  Shopping: {
+    label: "Shopping",
+    icon: "cart",
+    color: "#AB47BC",
+    gradient: "#8E24AA",
+  },
+  Bills: {
+    label: "Bills & Utilities",
+    icon: "flash",
+    color: "#FFA726",
+    gradient: "#FB8C00",
+  },
   Entertainment: {
     label: "Entertainment",
     icon: "game-controller",
     color: "#26C6DA",
+    gradient: "#00ACC1",
   },
-  HealthCare: { label: "HealthCare", icon: "fitness", color: "#EF5350" },
-  Education: { label: "Education", icon: "school", color: "#66BB6A" },
+  HealthCare: {
+    label: "HealthCare",
+    icon: "fitness",
+    color: "#EF5350",
+    gradient: "#D32F2F",
+  },
+  Education: {
+    label: "Education",
+    icon: "school",
+    color: "#66BB6A",
+    gradient: "#43A047",
+  },
   Other: {
     label: "Other Expense",
     icon: "ellipsis-horizontal",
     color: "#BDBDBD",
+    gradient: "#9E9E9E",
   },
-  Income: { label: "Income", icon: "cash", color: "#90EE90" },
+  Income: {
+    label: "Income",
+    icon: "cash",
+    color: "#90EE90",
+    gradient: "#66BB6A",
+  },
 };
